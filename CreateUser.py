26e341fc1138a26f64e3ee8ff1fe31becfbfa087
color.ui <53875838+alexyeganyan@users.noreply.github.com>
2020-06-08 23:18:04 +0400

    Final version create user, Add slider actions in T-Shirt.py

--- CreateUser.py
+++ CreateUser.py
@@ -3,6 +3,7 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
+import time
 
 # Create a instance of Firefox Browser
 driver = webdriver.Firefox()
@@ -11,90 +12,93 @@ driver = webdriver.Firefox()
 driver.get("http://automationpractice.com/index.php")
 driver.implicitly_wait(10)
 # driver.findElement(By.id("core-navigation")
-signLink = driver.find_element_by_css_selector(".login")
+signLink = driver.find_element_by_css_selector(".header_user_info , .login")
 signLink.click()
 
 # Enter valid email address
-emailAddress = driver.find_element_by_xpath(".//*[@id='email_create']")
+emailAddress = driver.find_element_by_css_selector("div.columns-container  #email_create")
 # emailAddress.send_keys("aaa@gg.com")
 emailAddress.send_keys("bbb@gg.com")
 # Click on create account
-createAccount = driver.find_element_by_xpath(".//*[@class='icon-user left']")
+createAccount = driver.find_element_by_css_selector("button#SubmitCreate")
 createAccount.click()
-# Check Ms option
-gender = driver.find_element_by_xpath(".//*[@id='id_gender2']")
+# Check Mr. option
+gender = driver.find_element_by_css_selector("div.radio-inline #id_gender1")
 gender.click()
 # Enter first name
-firstName = driver.find_element_by_xpath(".//*[@id='customer_firstname']")
+firstName = driver.find_element_by_css_selector("div.account_creation #customer_firstname")
 firstName.send_keys("Nocholas")
 # Enter last name
-lastName = driver.find_element_by_xpath(".//*[@id='customer_lastname']")
+lastName = driver.find_element_by_css_selector("div.account_creation #customer_lastname")
 lastName.send_keys("Johnson")
 # Enter password
-password = driver.find_element_by_xpath(".//*[@id='passwd']")
+password = driver.find_element_by_css_selector("div.account_creation #passwd")
 password.send_keys("Johnson")
 # Enter date
-days = Select(driver.find_element_by_xpath(".//*[@id='days']"))
-days.select_by_value("14")
-months = Select(driver.find_element_by_xpath(".//*[@id='months']"))
-months.select_by_value("3")
-years = Select(driver.find_element_by_xpath(".//*[@id='years']"))
-years.select_by_value("2001")
-# Check "Sign up for newsletter" AND "Recieve special offers"
-newsLetter = driver.find_element_by_xpath(".//*[@id='newsletter']")
-newsLetter.click()
-specialOffer = driver.find_element_by_xpath(".//*[@id='optin']")
-specialOffer.click()
-# Enter address details
-# Enter first name
-firstName = driver.find_element_by_xpath(".//*[@id='firstname']")
-firstName.send_keys("Bernard")
-# Enter last name
-lastName = driver.find_element_by_xpath(".//*[@id='lastname']")
-lastName.send_keys("Lampard")
-# Enter company name
-company = driver.find_element_by_xpath(".//*[@id='company']")
-company.send_keys("Foormols")
-# Enter address
-address1 = driver.find_element_by_xpath(".//*[@id='address1']")
-address1.send_keys("1622 E Ayre Street")
-# Enter address
-address2 = driver.find_element_by_xpath(".//*[@id='address2']")
-address2.send_keys("Sas1120")
-# Enter city
-city = driver.find_element_by_xpath(".//*[@id='city']")
-city.send_keys("Honk-Kong")
-# Enter state
-state = Select(driver.find_element_by_xpath(".//*[@id='id_state']"))
-state.select_by_value("5")
-# Enter zip code
-zipCode = driver.find_element_by_xpath(".//*[@id='postcode']")
-zipCode.send_keys("15444")
-# Enter country
-country = Select(driver.find_element_by_xpath(".//*[@id='id_country']"))
-country.select_by_value("21")
-# Enter additional info
-additional = driver.find_element_by_xpath(".//*[@id='other']")
-additional.send_keys("I am an engineer\nI live in Yerevan")
-# Enter home phone
-homePhone = driver.find_element_by_xpath(".//*[@id='phone']")
-homePhone.send_keys("+37455997711")
-# Enter home phone
-mobilePhone = driver.find_element_by_xpath(".//*[@id='phone_mobile']")
-mobilePhone.send_keys("+37455883300")
-# Enter email address for future references
-emailReference = driver.find_element_by_xpath(".//*[@id='alias']")
-emailReference.send_keys("Mango@ss.com")
-# // Register all data
-# register = driver.find_element_by_xpath(".//*[@id='submitAccount']")
-# register.click()
+days = Select(driver.find_element_by_css_selector("div#uniform-days #days"))
+days.select_by_visible_text("11  ")
+months = Select(driver.find_element_by_css_selector("div#uniform-months #months"))
+months.select_by_visible_text("May ")
+years = Select(driver.find_element_by_css_selector("div#uniform-years #years"))
+years.select_by_visible_text("2001  ")
+# # Check "Sign up for newsletter" AND "Recieve special offers"
+# newsLetter = driver.find_element_by_css_selector(".//*[@id='newsletter']")
+# newsLetter.click()
+# specialOffer = driver.find_element_by_css_selector(".//*[@id='optin']")
+# specialOffer.click()
+# # Enter address details
+# # Enter first name
+# firstName = driver.find_element_by_css_selector(".//*[@id='firstname']")
+# firstName.send_keys("Bernard")
+# # Enter last name
+# lastName = driver.find_element_by_css_selector(".//*[@id='lastname']")
+# lastName.send_keys("Lampard")
+# # Enter company name
+# company = driver.find_element_by_css_selector(".//*[@id='company']")
+# company.send_keys("Foormols")
+# # Enter address
+# address1 = driver.find_element_by_css_selector(".//*[@id='address1']")
+# address1.send_keys("1622 E Ayre Street")
+# # Enter address
+# address2 = driver.find_element_by_css_selector(".//*[@id='address2']")
+# address2.send_keys("Sas1120")
+# # Enter city
+# city = driver.find_element_by_css_selector(".//*[@id='city']")
+# city.send_keys("Honk-Kong")
+# # Enter state
+# state = Select(driver.find_element_by_css_selector(".//*[@id='id_state']"))
+# state.select_by_value("5")
+# # Enter zip code
+# zipCode = driver.find_element_by_css_selector(".//*[@id='postcode']")
+# zipCode.send_keys("15444")
+# # Enter country
+# country = Select(driver.find_element_by_css_selector(".//*[@id='id_country']"))
+# country.select_by_value("21")
+# # Enter additional info
+# additional = driver.find_element_by_css_selector(".//*[@id='other']")
+# additional.send_keys("I am an engineer\nI live in Yerevan")
+# # Enter home phone
+# homePhone = driver.find_element_by_css_selector(".//*[@id='phone']")
+# homePhone.send_keys("+37455997711")
+# # Enter home phone
+# mobilePhone = driver.find_element_by_css_selector(".//*[@id='phone_mobile']")
+# mobilePhone.send_keys("+37455883300")
+# # Enter email address for future references
+# emailReference = driver.find_element_by_css_selector(".//*[@id='alias']")
+# emailReference.send_keys("Mango@ss.com")
+# # // Register all data
+# # register = driver.find_element_by_css_selector(".//*[@id='submitAccount']")
+# # register.click()
+#
+# # Maximize the Browser window
+# # driver.manage().window().maximize()
+# #
+# # // Get the current page URL and store the value in variable 'str'
+# # String str = driver.getCurrentUrl()
+# #
+# # // Print the value of variable in the console
+# # System.out.println("The current URL is " + str)
+# # // System.out.println(searchText.getText()
 
-# Maximize the Browser window
-# driver.manage().window().maximize()
-#
-# // Get the current page URL and store the value in variable 'str'
-# String str = driver.getCurrentUrl()
-#
-# // Print the value of variable in the console
-# System.out.println("The current URL is " + str)
-# // System.out.println(searchText.getText()
+time.sleep(3)
+driver.close()
--- TShirt.py
+++ TShirt.py
@@ -1,22 +1,54 @@
 #!/usr/bin/env python3
+import time
 
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 
 # Create a instance of Firefox Browser
-driver = webdriver.Firefox()
+# driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 
 # Open the URL in firefox browser
 driver.get("http://automationpractice.com/index.php")
+# Maximize the Browser window
+driver.maximize_window()
+driver.implicitly_wait(10)
 # driver.implicitly_wait(10)
-# TShirt = driver.find_element_by_css_selector(".sf-menu > li:nth-child(3) > a:nth-child(1)")
-TShirt = driver.find_element().by
-# TShirt = driver.find_element_by_xpath("/html/body/div/div[1]/header/div[3]/div/div/div[6]/ul/li[3]/a")
-# TShirt = driver.find_element_by_xpath(".//*[@title='T-shirts']")
+TShirt = driver.find_element_by_xpath(".//*[@id='block_top_menu']/ul/li/a[@title='T-shirts']")
 TShirt.click()
 # Select sort criteria
 selectProductSort = Select(driver.find_element_by_xpath(".//*[@id='selectProductSort']"))
 selectProductSort.select_by_value("reference:asc")
-#selectProductSort
-#selectProductSort > option:nth-child(7)
+# Check product's size
+size = driver.find_element_by_css_selector("#ul_layered_id_attribute_group_1 input#layered_id_attribute_group_3")
+size.click()
+# Check product's composition
+composition = driver.find_element_by_css_selector("#ul_layered_id_feature_5.col-lg-12 div#uniform-layered_id_feature_5")
+composition.click()
+# Check product's condition
+condition = driver.find_element_by_css_selector("#ul_layered_condition_0 #uniform-layered_condition_new")
+condition.click()
+# Select price range
+priceSlider = driver.find_element_by_css_selector(".layered_price #layered_price_slider")
+firstSlider = priceSlider.find_element_by_css_selector("[style='left: 0%;']")
+lastSlider = priceSlider.find_element_by_css_selector("[style='left: 100%;']")
+firstSliderCoord = firstSlider.location
+lastSliderCoord = lastSlider.location
+# Obtain X and Y coordinates
+x1Coord = firstSliderCoord.get("x")
+x2Coord = lastSliderCoord.get("x")
+delta = x2Coord - x1Coord
+# move sliders from two sides
+actions = ActionChains(driver)
+actions.move_to_element(firstSlider)
+actions.drag_and_drop_by_offset(firstSlider, int(0.2 * delta), 0)
+actions.perform()
+print(int((-0.15) * delta))
+actions.move_to_element(lastSlider)
+actions.drag_and_drop_by_offset(lastSlider, int((-0.15) * delta), 0)
+actions.perform()
+
+time.sleep(5)
+driver.close()
