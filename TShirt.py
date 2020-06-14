6a21c3056c727ba03efd97d270246e489922caaf
color.ui <53875838+alexyeganyan@users.noreply.github.com>
2020-06-14 12:00:43 +0400

    Sign to Facebook with cookies, minor changes in 'TShirt.py'

--- TShirt.py
+++ TShirt.py
@@ -10,16 +10,16 @@ from selenium.webdriver.support.ui import Select
 # driver = webdriver.Firefox()
 driver = webdriver.Chrome()
 
-# Open the URL in firefox browser
+# Open the URL in the browser
 driver.get("http://automationpractice.com/index.php")
-# Maximize the Browser window
+# Maximize the browser window
 driver.maximize_window()
 driver.implicitly_wait(10)
-# driver.implicitly_wait(10)
+# Click on T-shirts' link
 TShirt = driver.find_element_by_xpath(".//*[@id='block_top_menu']/ul/li/a[@title='T-shirts']")
 TShirt.click()
 # Select sort criteria
-selectProductSort = Select(driver.find_element_by_xpath(".//*[@id='selectProductSort']"))
+selectProductSort = Select(driver.find_element_by_css_selector("#productsSortForm #selectProductSort"))
 selectProductSort.select_by_value("reference:asc")
 # Check product's size
 size = driver.find_element_by_css_selector("#ul_layered_id_attribute_group_1 input#layered_id_attribute_group_3")
@@ -31,24 +31,40 @@ composition.click()
 condition = driver.find_element_by_css_selector("#ul_layered_condition_0 #uniform-layered_condition_new")
 condition.click()
 # Select price range
-priceSlider = driver.find_element_by_css_selector(".layered_price #layered_price_slider")
-firstSlider = priceSlider.find_element_by_css_selector("[style='left: 0%;']")
-lastSlider = priceSlider.find_element_by_css_selector("[style='left: 100%;']")
-firstSliderCoord = firstSlider.location
-lastSliderCoord = lastSlider.location
-# Obtain X and Y coordinates
-x1Coord = firstSliderCoord.get("x")
-x2Coord = lastSliderCoord.get("x")
-delta = x2Coord - x1Coord
-# move sliders from two sides
-actions = ActionChains(driver)
-actions.move_to_element(firstSlider)
-actions.drag_and_drop_by_offset(firstSlider, int(0.2 * delta), 0)
-actions.perform()
-print(int((-0.15) * delta))
-actions.move_to_element(lastSlider)
-actions.drag_and_drop_by_offset(lastSlider, int((-0.15) * delta), 0)
-actions.perform()
+print(driver.name)
+if driver.name == "chrome":
+    priceSlider = driver.find_element_by_css_selector(".layered_price #layered_price_slider")
+    firstSlider = priceSlider.find_element_by_css_selector("[style='left: 0%;']")
+    lastSlider = priceSlider.find_element_by_css_selector("[style='left: 100%;']")
+    firstSliderCoord = firstSlider.location
+    lastSliderCoord = lastSlider.location
+    # Obtain X and Y coordinates
+    x1Coord = firstSliderCoord.get("x")
+    x2Coord = lastSliderCoord.get("x")
+    delta = x2Coord - x1Coord
+    # move sliders from two sides
+    actions = ActionChains(driver)
+    actions.move_to_element(firstSlider)
+    actions.drag_and_drop_by_offset(firstSlider, int(0.2 * delta), 0)
+    actions.perform()
+    actions.move_to_element(lastSlider)
+    actions.drag_and_drop_by_offset(lastSlider, int((-0.15) * delta), 0)
+    actions.perform()
+
+# # Open a wearing with the certain name
+# wearing = driver.find_element_by_css_selector(".product-container a.product-name[title='Faded Short Sleeve T-shirts']")
+# wearing.click()
+if driver.name == "chrome":
+    # Add to cart a wearing with the certain name
+    wearing = driver.find_element_by_css_selector(".product-container a.product-name[title='Faded Short Sleeve T-shirts']")
+    actions = ActionChains(driver)
+    actions.move_to_element(wearing)
+    actions.perform()
+    cart = driver.find_element_by_css_selector(".product-container .ajax_add_to_cart_button.btn[title='Add to cart']")
+    cart.click()
+    # Proceed to checkout
+    checkout = driver.find_element_by_css_selector("#layer_cart .clearfix a[title='Proceed to checkout']")
+    checkout.click()
 
 time.sleep(5)
 driver.close()
--- Test.py
+++ Test.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
+import time
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 # Step 1) Open Firefox
+# browser = webdriver.Chrome()
 browser = webdriver.Firefox()
 # Step 2) Navigate to Facebook
 browser.get("http://www.facebook.com")
-# Step 3) Search & Enter the Email or Phone field & Enter Password
-username = browser.find_element_by_id("email")
-password = browser.find_element_by_id("pass")
-submit   = browser.find_element_by_id("loginbutton")
-username.send_keys("YOUR EMAILID")
-password.send_keys("YOUR PASSWORD")
-# Step 4) Click Login
-submit.click()
-wait = WebDriverWait( browser, 5 )
+# Add cookies to sign in with the user
+browser.add_cookie({'name': 'c_user', 'value': '100001829131180'})
+browser.add_cookie({'name': 'xs', 'value': '17%3AQ_NNMgaNa3F5Gw%3A2%3A1592077663%3A12249%3A3687'})
+# Reload page to open it with with the signed information cookies
+browser.get("http://www.facebook.com")
+
 page_title = browser.title
 assert page_title == "Facebook"
+
+time.sleep(5)
+browser.close()
