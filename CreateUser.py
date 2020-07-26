b22c49367e5834dd2815c2707e67e92e3532afda
color.ui <53875838+alexyeganyan@users.noreply.github.com>
2020-07-26 21:58:04 +0400

    Refactor TShirt and CreateUser fles

--- CreateUser.py
+++ CreateUser.py
@@ -5,91 +5,121 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 import time
 
-# Create a instance of Firefox Browser
-driver = webdriver.Firefox()
 
-# Open the URL in firefox browser
-driver.get("http://automationpractice.com/index.php")
-driver.implicitly_wait(10)
-# driver.findElement(By.id("core-navigation")
-driver.find_element_by_css_selector(".header_user_info , .login").click()
+def enter_email_address(address):
+    email_address = driver.find_element_by_css_selector("div.columns-container  #email_create")
+    email_address.clear()
+    email_address.send_keys(address)
+    # Click on create account
+    driver.find_element_by_css_selector("button#SubmitCreate").click()
 
+
+def enter_name_password_gender(firstname, lastname, password):
+    # Check Mr. option
+    driver.find_element_by_css_selector("div.radio-inline #id_gender1").click()
+    # Enter first name
+    driver.find_element_by_css_selector("div.account_creation #customer_firstname").send_keys(firstname)
+    # Enter last name
+    driver.find_element_by_css_selector("div.account_creation #customer_lastname").send_keys(lastname)
+    # Enter password
+    driver.find_element_by_css_selector("div.account_creation #passwd").send_keys(password)
+
+
+def enter_date(day, month, year):
+    Select(driver.find_element_by_css_selector("div#uniform-days #days")).select_by_visible_text(day)
+    Select(driver.find_element_by_css_selector("div#uniform-months #months")).select_by_visible_text(month)
+    Select(driver.find_element_by_css_selector("div#uniform-years #years")).select_by_visible_text(year)
+
+
+def valid_inner_text(elem):
+    return elem.find_element_by_css_selector("#create_account_error.alert.alert-danger").get_attribute("innerText")
+
+
+def check_error_text():
+    # Wait until error text is appeared
+    WebDriverWait(driver, 15).until(lambda x: valid_inner_text(x))
+    inner_text = driver.find_element_by_css_selector("#create_account_error.alert.alert-danger").get_attribute(
+        "innerText")
+    assert inner_text == "Invalid email address."
+
+
+def open_browser(url, browser):
+    global driver
+    # Create a instance of Browser
+    if browser == "chrome":
+        driver = webdriver.Chrome()
+    elif browser == "firefox":
+        driver = webdriver.Firefox()
+    # Open the URL in the browser
+    driver.get(url)
+    driver.implicitly_wait(10)
+    driver.find_element_by_css_selector(".header_user_info , .login").click()
+
+
+def enter_firstname_lastname_company(first_name, last_name, company):
+    # Enter first name
+    driver.find_element_by_xpath("//*[@id='firstname']").send_keys(first_name)
+    # Enter last name
+    driver.find_element_by_xpath(".//*[@id='lastname']").send_keys(last_name)
+    # Enter company name
+    driver.find_element_by_css_selector("#company").send_keys(company)
+
+
+def enter_addresses_city_state(address, address2, city, state):
+    # Enter address
+    driver.find_element_by_css_selector("#address1").send_keys(address)
+    # Enter address 2
+    driver.find_element_by_css_selector("#address2").send_keys(address2)
+    # Enter city
+    driver.find_element_by_css_selector("#city").send_keys(city)
+    # Enter state
+    Select(driver.find_element_by_xpath(".//*[@id='id_state']")).select_by_visible_text(state)
+
+
+def enter_zip_country_info(zip_code, country, additional_info):
+    # Enter zip code
+    driver.find_element_by_xpath(".//*[@id='postcode']").send_keys(zip_code)
+    # Enter country
+    Select(driver.find_element_by_css_selector("#id_country")).select_by_value(country)
+    # Enter additional info
+    driver.find_element_by_css_selector("#other").send_keys(additional_info)
+
+
+def enter_phones_email_reference(home_phone, mobile_phone, email_reference):
+    # Enter home phone
+    driver.find_element_by_css_selector("#phone").send_keys(home_phone)
+    # Enter mobile phone
+    driver.find_element_by_css_selector("#phone_mobile").send_keys(mobile_phone)
+    # Enter email address for future references
+    driver.find_element_by_css_selector("#alias").send_keys(email_reference)
+
+
+def close_browser(timeout):
+    time.sleep(timeout)
+    driver.close()
+
+
+def sign_up_newsletter_special_offer():
+    driver.find_element_by_css_selector("#newsletter").click()
+    driver.find_element_by_css_selector("#optin").click()
+
+
+open_browser(url="http://automationpractice.com/index.php", browser="firefox")
 # Enter invalid email address and check error message
-emailAddress = driver.find_element_by_css_selector("div.columns-container  #email_create").send_keys("bbb@.g.com")
-
-# Click on create account
-driver.find_element_by_css_selector("button#SubmitCreate").click()
-# Wait until error text is appeared
-
-
-def len_inner_text(elem):
-    return len(elem.find_element_by_css_selector("#create_account_error.alert.alert-danger").get_attribute("innerText"))
-
-
-WebDriverWait(driver, 15).until(lambda x: len_inner_text(x) > 0)
-
-inner_text = driver.find_element_by_css_selector("#create_account_error.alert.alert-danger").get_attribute("innerText")
-assert inner_text == "Invalid email address."
-
+enter_email_address("bbb@.g.com")
+check_error_text()
 # Enter valid email address
-emailAddress = driver.find_element_by_css_selector("div.columns-container  #email_create")
-emailAddress.clear()
-emailAddress.send_keys("bbb@gg.com")
-# emailAddress.send_keys("aaa@gg.com")
-# Click on create account
-driver.find_element_by_css_selector("button#SubmitCreate").click()
-# Check Mr. option
-driver.find_element_by_css_selector("div.radio-inline #id_gender1").click()
-# Enter first name
-driver.find_element_by_css_selector("div.account_creation #customer_firstname").send_keys("Nocholas")
-# Enter last name
-driver.find_element_by_css_selector("div.account_creation #customer_lastname").send_keys("Johnson")
-# Enter password# Enter password
-driver.find_element_by_css_selector("div.account_creation #passwd").send_keys("Johnson")
-# Enter date
-Select(driver.find_element_by_css_selector("div#uniform-days #days")).select_by_visible_text("11  ")
-Select(driver.find_element_by_css_selector("div#uniform-months #months")).select_by_visible_text("May ")
-Select(driver.find_element_by_css_selector("div#uniform-years #years")).select_by_visible_text("2001  ")
-# Check "Sign up for newsletter" AND "Recieve special offers"
-newsLetter = driver.find_element_by_css_selector("#newsletter").click()
-driver.find_element_by_css_selector("#optin").click()
-# Enter address details
-# Enter first name
-driver.find_element_by_xpath("//*[@id='firstname']").send_keys("Bernard")
-# Enter last name
-driver.find_element_by_xpath(".//*[@id='lastname']").send_keys("Lampard")
-# Enter company name
-driver.find_element_by_css_selector("#company").send_keys("Foormols")
-# Enter address
-driver.find_element_by_css_selector("#address1").send_keys("1622 E Ayre Street")
-# Enter address
-driver.find_element_by_css_selector("#address2").send_keys("Sas1120")
-# Enter city
-driver.find_element_by_css_selector("#city").send_keys("Honk-Kong")
-# Enter state
-Select(driver.find_element_by_xpath(".//*[@id='id_state']")).select_by_value("5")
-# Enter zip code
-driver.find_element_by_xpath(".//*[@id='postcode']").send_keys("15444")
-# Enter country
-Select(driver.find_element_by_css_selector("#id_country")).select_by_value("21")
-# Enter additional info
-driver.find_element_by_css_selector("#other").send_keys("I am an engineer\nI live in Yerevan")
-# Enter home phone
-driver.find_element_by_css_selector("#phone").send_keys("+37455997711")
-# Enter home phone
-driver.find_element_by_css_selector("#phone_mobile").send_keys("+37455883300")
-# Enter email address for future references
-driver.find_element_by_css_selector("#alias").send_keys("Mango@ss.com")
-# # // Register all data
-# # register = driver.find_element_by_css_selector(".//*[@id='submitAccount']")
-# # register.click()
-# #
-# # // Get the current page URL and store the value in variable 'str'
-# # String str = driver.getCurrentUrl()
-# #
-# # // Print the value of variable in the console
-# # System.out.println("The current URL is " + str)
-# # // System.out.println(searchText.getText()
-#
-time.sleep(3)
-driver.close()
+enter_email_address("bbb@gg.com")
+enter_name_password_gender(firstname="Nicholas", lastname="Johnson", password="Johnson")
+enter_date(day="11  ", month="May ", year="2001  ")
+sign_up_newsletter_special_offer()
+# Enter address information
+enter_firstname_lastname_company(first_name="Bernard", last_name="Lampard", company="Foormols")
+enter_addresses_city_state(address="1622 E Ayre Street", address2="Sas1120", city="Honk-Kong", state="Arizona")
+enter_zip_country_info(zip_code="15444", country="21", additional_info="I am an engineer\nI live in Yerevan")
+enter_phones_email_reference(home_phone="+37455997711", mobile_phone="+37455883300", email_reference="Mango@ss.com")
+
+# Register all data
+# driver.find_element_by_css_selector(".//*[@id='submitAccount']").click()
+
+close_browser(timeout=3)
--- TShirt.py
+++ TShirt.py
@@ -6,65 +6,87 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 
-# Create a instance of Firefox Browser
-# driver = webdriver.Firefox()
-driver = webdriver.Chrome()
 
-# Open the URL in the browser
-driver.get("http://automationpractice.com/index.php")
-# Maximize the browser window
-driver.maximize_window()
-driver.implicitly_wait(10)
-# Click on T-shirts' link
-TShirt = driver.find_element_by_xpath(".//*[@id='block_top_menu']/ul/li/a[@title='T-shirts']")
-TShirt.click()
-# Select sort criteria
-selectProductSort = Select(driver.find_element_by_css_selector("#productsSortForm #selectProductSort"))
-selectProductSort.select_by_value("reference:asc")
-# Check product's size
-size = driver.find_element_by_css_selector("#ul_layered_id_attribute_group_1 input#layered_id_attribute_group_3")
-size.click()
-# Check product's composition
-composition = driver.find_element_by_css_selector("#ul_layered_id_feature_5.col-lg-12 div#uniform-layered_id_feature_5")
-composition.click()
-# Check product's condition
-condition = driver.find_element_by_css_selector("#ul_layered_condition_0 #uniform-layered_condition_new")
-condition.click()
-# Select price range
-print(driver.name)
-if driver.name == "chrome":
-    priceSlider = driver.find_element_by_css_selector(".layered_price #layered_price_slider")
-    firstSlider = priceSlider.find_element_by_css_selector("[style='left: 0%;']")
-    lastSlider = priceSlider.find_element_by_css_selector("[style='left: 100%;']")
-    firstSliderCoord = firstSlider.location
-    lastSliderCoord = lastSlider.location
-    # Obtain X and Y coordinates
-    x1Coord = firstSliderCoord.get("x")
-    x2Coord = lastSliderCoord.get("x")
-    delta = x2Coord - x1Coord
-    # move sliders from two sides
-    actions = ActionChains(driver)
-    actions.move_to_element(firstSlider)
-    actions.drag_and_drop_by_offset(firstSlider, int(0.2 * delta), 0)
-    actions.perform()
-    actions.move_to_element(lastSlider)
-    actions.drag_and_drop_by_offset(lastSlider, int((-0.15) * delta), 0)
-    actions.perform()
+def open_browser(url, browser):
+    global driver
+    # Create a instance of Browser
+    if browser == "chrome":
+        driver = webdriver.Chrome()
+    elif browser == "firefox":
+        driver = webdriver.Firefox()
+    # Open the URL in the browser
+    driver.get(url)
+    driver.maximize_window()
+    driver.implicitly_wait(10)
 
-# # Open a wearing with the certain name
-# wearing = driver.find_element_by_css_selector(".product-container a.product-name[title='Faded Short Sleeve T-shirts']")
-# wearing.click()
-if driver.name == "chrome":
-    # Add to cart a wearing with the certain name
-    wearing = driver.find_element_by_css_selector(".product-container a.product-name[title='Faded Short Sleeve T-shirts']")
-    actions = ActionChains(driver)
-    actions.move_to_element(wearing)
-    actions.perform()
-    cart = driver.find_element_by_css_selector(".product-container .ajax_add_to_cart_button.btn[title='Add to cart']")
-    cart.click()
-    # Proceed to checkout
-    checkout = driver.find_element_by_css_selector("#layer_cart .clearfix a[title='Proceed to checkout']")
-    checkout.click()
 
-time.sleep(5)
-driver.close()
+def click_tshirt_sort():
+    # Click on T-shirts' link
+    driver.find_element_by_xpath(".//*[@id='block_top_menu']/ul/li/a[@title='T-shirts']").click()
+    # Select sort criteria
+    Select(driver.find_element_by_css_selector("#productsSortForm #selectProductSort")).select_by_value("reference:asc")
+
+
+def product_size_composition_condition():
+    # Check product's size
+    driver.find_element_by_css_selector("#ul_layered_id_attribute_group_1 input#layered_id_attribute_group_3").click()
+    # Check product's composition
+    driver.find_element_by_css_selector("#ul_layered_id_feature_5.col-lg-12 div#uniform-layered_id_feature_5").click()
+    # Check product's condition
+    driver.find_element_by_css_selector("#ul_layered_condition_0 #uniform-layered_condition_new").click()
+    # Select price range
+
+
+def change_price_range_with_slider():
+    global actions
+    if driver.name == "chrome":
+        price_slider = driver.find_element_by_css_selector(".layered_price #layered_price_slider")
+        first_slider = price_slider.find_element_by_css_selector("[style='left: 0%;']")
+        last_slider = price_slider.find_element_by_css_selector("[style='left: 100%;']")
+        first_slider_coord = first_slider.location
+        last_slider_coord = last_slider.location
+        print(first_slider_coord)
+        print(last_slider_coord)
+        # Obtain X and Y coordinates
+        x1_coord = first_slider_coord.get("x")
+        x2_coord = last_slider_coord.get("x")
+        delta = x2_coord - x1_coord
+        # move sliders from two sides
+        actions = ActionChains(driver)
+        actions.move_to_element(first_slider)
+        actions.drag_and_drop_by_offset(first_slider, int(0.2 * delta), 0)
+        actions.perform()
+        actions.move_to_element(last_slider)
+        actions.drag_and_drop_by_offset(last_slider, int((-0.15) * delta), 0)
+        actions.perform()
+
+
+def open_wearing_name():
+    global actions
+    # Open a wearing with the certain name
+    if driver.name == "chrome":
+        # Add to cart a wearing with the certain name
+        wearing = driver.find_element_by_css_selector(
+            ".product-container a.product-name[title='Faded Short Sleeve T-shirts']")
+        actions = ActionChains(driver)
+        actions.move_to_element(wearing)
+        actions.perform()
+        cart = driver.find_element_by_css_selector(
+            ".product-container .ajax_add_to_cart_button.btn[title='Add to cart']")
+        cart.click()
+        # Proceed to checkout
+        checkout = driver.find_element_by_css_selector("#layer_cart .clearfix a[title='Proceed to checkout']")
+        checkout.click()
+
+
+def close_browser():
+    time.sleep(3)
+    driver.close()
+
+
+open_browser(url="http://automationpractice.com/index.php", browser="chrome")
+click_tshirt_sort()
+product_size_composition_condition()
+change_price_range_with_slider()
+open_wearing_name()
+close_browser()
